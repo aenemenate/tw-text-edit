050c7f4df761254c8bf66ef40d95b1d0db80b8f0
Tristan Williams <aenemenate@localhost.localdomain>
2020-10-17 14:38:25 -0700

    undo: add submodule

--- src/clipboard.cpp
+++ src/clipboard.cpp
@@ -1,12 +1,37 @@
 #include "clipboard.h"
-#include <clip.h>
+
+#if defined(_WIN32) || defined(_WIN64)
+  #include <windows.h>
+#endif
 
 std::string GetClipboardContents() {
-  std::string clip_content;
-  clip::get_text(clip_content);
-  return clip_content;
+#if defined(_WIN32) || defined(_WIN64)
+  std::string text;
+  if (OpenClipboard(nullptr)) {
+    HANDLE hData = GetClipboardData(CF_TEXT);
+    if (hData != nullptr) {
+      char * pszText = static_cast<char*>( GlobalLock(hData) );
+      if (pszText != nullptr)
+        text = pszText;
+      GlobalUnlock(hData);
+      CloseClipboard();
+    }
+  }
+  return text;
+#endif
+  return std::string("");
 }
 
 void PushTextToClipboard(std::string text) {
-  clip::set_text(text);
+#if defined(_WIN32) || defined(_WIN64)
+  const char* output = text.c_str();
+  const size_t len = strlen(output) + 1;
+  HGLOBAL hMem =  GlobalAlloc(GMEM_MOVEABLE, len);
+  memcpy(GlobalLock(hMem), output, len);
+  GlobalUnlock(hMem);
+  OpenClipboard(0);
+  EmptyClipboard();
+  SetClipboardData(CF_TEXT, hMem);
+  CloseClipboard();
+#endif
 }
