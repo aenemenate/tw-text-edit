bfe5643353f02f940f994ee614583397f050c4c6
Tristan Williams <aenemenate@localhost.localdomain>
2020-10-17 13:09:54 -0700

    update: bearlibterminal submodule

--- src/clipboard.cpp
+++ src/clipboard.cpp
@@ -1,35 +1,12 @@
 #include "clipboard.h"
-#if defined(_WIN32) || defined(_WIN64)
-  #include <windows.h>
-#endif
+#include <clip.h>
+
 std::string GetClipboardContents() {
-#if defined(_WIN32) || defined(_WIN64)
-  std::string text;
-  if (OpenClipboard(nullptr)) {
-    HANDLE hData = GetClipboardData(CF_TEXT);
-    if (hData != nullptr) {
-      char * pszText = static_cast<char*>( GlobalLock(hData) );
-      if (pszText != nullptr)
-        text = pszText;
-      GlobalUnlock(hData);
-      CloseClipboard();
-    }
-  }
-  return text;
-#endif
-  return std::string("");
+  std::string clip_content;
+  clip::get_text(clip_content);
+  return clip_content;
 }
 
 void PushTextToClipboard(std::string text) {
-#if defined(_WIN32) || defined(_WIN64)
-  const char* output = text.c_str();
-  const size_t len = strlen(output) + 1;
-  HGLOBAL hMem =  GlobalAlloc(GMEM_MOVEABLE, len);
-  memcpy(GlobalLock(hMem), output, len);
-  GlobalUnlock(hMem);
-  OpenClipboard(0);
-  EmptyClipboard();
-  SetClipboardData(CF_TEXT, hMem);
-  CloseClipboard();
-#endif
+  clip::set_text(text);
 }
